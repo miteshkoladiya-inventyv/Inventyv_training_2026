76fd876f409fc97e7705e8e11263ee70fa4d0427
miteshkoladiya <miteshkoladiya1117@gmail.com>
2026-01-23 14:58:21 +0530

    js task added

---
+++ 04_JS_Task/01_task.js
@@ -0,0 +1,24 @@
+function firstfn(arr, callback) {
+    const firstElement = arr.shift();
+    return callback(firstElement, arr);
+}
+
+function secondfn(firstElement, restOfArray) {  
+    return firstElement + restOfArray.reduce((acc, val) => acc + val, 0);
+}
+
+
+const arr1 = [10, 15, 5, 8];
+const sum = firstfn(arr1, secondfn);
+
+new Promise((resolve, reject) => {
+    if (sum > 35) {
+        resolve(`Sum is ${sum} , which is greater than 35.`);
+    }
+    else {      
+        reject(`Sum is ${sum} , which is not greater than 35.`);
+    }
+})
+.then(msg => console.log("Resolved:", msg))
+.catch(err => console.log("Rejected:", err));
+
